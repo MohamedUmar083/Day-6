c5d8623071555865d78bf66280676c70c738ade0
MohamedUmar083 <umarmohd777@gmail.com>
2024-03-29 22:54:14 +0530

    Day-6/Tasks

---
+++ script.js
@@ -0,0 +1,39 @@
+//Task - 2
+/*
+class Circle{
+    constructor(radius,color){
+        this.radius = radius;
+        this.color = color;
+    }
+    get Radius(){
+        return this.radius;
+    }
+    set Radius(r){
+        this.radius = r;
+    }
+    get Color(){
+        return this.color;
+    }
+    set Color(c){
+        this.color = c;
+    }
+    get toString(){
+        return `"Circle[radius = ${this.radius}, color = ${this.color}]"`;
+    }
+    get Area(){
+        return Math.PI*Math.pow(this.radius,2);
+    }
+    get Circumference(){
+        return 2*Math.PI*this.radius
+    }
+}
+
+const circle = new Circle(1.0,"red");
+console.log(circle.radius,circle.color);
+
+console.log(circle.Radius);
+console.log(circle.Color);
+console.log(circle.toString);
+console.log(circle.Area);
+console.log(circle.Circumference);
+*/
