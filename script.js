a5e53d3bd06d5a8f847c01b453d122e8e6dbc364
MohamedUmar083 <umarmohd777@gmail.com>
2024-03-29 22:56:28 +0530

    Day-6/Tasks

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//Task - 2
 /*
+//Task - 2
+
 class Circle{
     constructor(radius,color){
         this.radius = radius;
@@ -36,4 +37,28 @@ console.log(circle.Color);
 console.log(circle.toString);
 console.log(circle.Area);
 console.log(circle.Circumference);
+*/
+
+
+/*
+// Task - 3
+
+class Person{
+
+    constructor(name,age,gender,m_status,contact,email){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.m_status = m_status;
+        this.contact = contact;
+        this.email = email;
+
+    }
+
+}
+
+const person1 = new Person("Umar",23,"male","single",9876543210,"abc@example.com");
+const person2 = new Person("wazim",23,"male","married",9876541230,"def@example.com");
+console.log(person1.name,person1.age,person1.gender,person1.m_status,person1.contact,person1.email);
+console.log(person2.name,person2.age,person2.gender,person2.m_status,person2.contact,person2.email);
 */
